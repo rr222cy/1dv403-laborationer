5d94d5e3fb44da97392c983c30afeb85bade477d
rr222cy <rr222cy@student.lnu.se>
2013-12-02 23:53:05 +0100

    Korrektion, kastar nu ett undantag vid fel

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -8,15 +8,22 @@ window.onload = function(){
 		// Returnera den konverterade strängen.
 	    // Vid fel, kasta ett undantag med ett meddelande till användaren.
 
-	    if (str === "")
+	    try 
 	    {
-	        return "Mata in en mening du vill omvandla!";
-	    }
-	    else
-	    {
-	        var strConverted = str.toLowerCase();
+	        if (str === "")
+	        {
+	            throw "Mata in en mening du vill omvandla!";
+	        }
+	        else
+	        {
+	            var strConverted = str.toLowerCase();
 
-	        return strConverted;
+	            return strConverted;
+	        }
+	    }
+	    catch(errorMsg)
+	    {
+	        return errorMsg;
 	    }
 	
 
