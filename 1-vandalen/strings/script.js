15b66df87f48006f6e83fd9b8a48a36edd3d1286
rr222cy <rr222cy@student.lnu.se>
2013-12-06 04:02:22 +0100

    Strings färdig

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -23,13 +23,23 @@ window.onload = function(){
                 // Loopar här igenom varje tecken i min sträng och utför önskad operation enligt if-satserna för respektive tecken.
                 for(i = 0; i<str.length; i++)
                 {
+                    // Ersätter alla 'a' och 'A' med '#'.
                     if(str.charAt(i) === 'a' || str.charAt(i) === 'A')
                     {
                         strConverted += '#';
+                        continue;
                     }
-                    else
+                    // Kollar om mitt tecken redan är lowercase, om så är fallet görs tecknet om till uppercase.
+                    else if (str.charAt(i) === str.charAt(i).toLocaleLowerCase())
                     {
-                        strConverted += str.charAt(i);
+                        strConverted += str.charAt(i).toLocaleUpperCase();
+                        continue;
+                    }
+                    // Samma som ovan, fast tvärtom
+                    else if (str.charAt(i) === str.charAt(i).toLocaleUpperCase())
+                    {
+                        strConverted += str.charAt(i).toLocaleLowerCase();
+                        continue;
                     }
                 }
 
