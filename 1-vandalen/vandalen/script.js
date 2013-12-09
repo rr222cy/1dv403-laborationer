0d4f3135f0252cecedf451edcae340d4d5b5eea3
rr222cy <rr222cy@student.lnu.se>
2013-12-09 07:05:12 +0100

    Räknar nu ut medelålder korrekt för Vandalen

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -8,11 +8,14 @@ var makePerson = function(persArr) {
     var averageAge;
     var names = [];
     var allAges = [];
+    var totalAge = 0;
         
     // Loopar ut alla åldrar och namn ur arrayen persArr, skickar sedan in resultaten i 2 nya arrayer så vi kan sortera dessa.
+    // Lägger även in alla åldrar i variabeln totalAge, så vi kan beräkna medelåldern.
     for (var i = 0; i < persArr.length; i++) 
     {
         allAges.push(persArr[i].age);
+        totalAge += persArr[i].age;
         names.push(persArr[i].name);
     }
     
@@ -20,7 +23,8 @@ var makePerson = function(persArr) {
     allAges.sort(function(a,b){return a - b;});
     minAge = allAges[0];
     maxAge = allAges[allAges.length-1];
-    averageAge = 40;
+    averageAge = Math.round(totalAge / allAges.length);
+   
     
     names.sort();
     
