a1fc3c003f45b6fadf978c11a47b62c5e19e08be
rr222cy <rr222cy@student.lnu.se>
2013-12-09 06:50:20 +0100

    Sorterat åldrar och namn för Vandalen

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,12 +2,27 @@
 
 var makePerson = function(persArr) {
    
-    var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
     var result = {};
     var maxAge;
     var minAge;
     var averageAge;
-    var names;
+    var names = [];
+    var allAges = [];
+        
+    // Loopar ut alla åldrar och namn ur arrayen persArr, skickar sedan in resultaten i 2 nya arrayer så vi kan sortera dessa.
+    for (var i = 0; i < persArr.length; i++) 
+    {
+        allAges.push(persArr[i].age);
+        names.push(persArr[i].name);
+    }
+    
+    // Sorterar här åldrarna så att vi får yngst först och äldst sist
+    allAges.sort(function(a,b){return a - b;});
+    minAge = allAges[0];
+    maxAge = allAges[allAges.length-1];
+    averageAge = 40;
+    
+    names.sort();
     
     result = {
         maxAge: maxAge,
