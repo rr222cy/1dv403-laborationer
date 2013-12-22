c150ebf71e42395e831bbc8c4890b06a0de6c7ce
rr222cy <rr222cy@student.lnu.se>
2013-12-22 15:17:25 +0100

    Initial JS-struktur
    Egentligen en extracommit/push pga backup/synk mellan datorer pga
    förlorad internetuppkoppling pga vädret.

---
+++ 2-labbymezzage/Message.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+
+// Lånar här in ForEach-metoden från Array
+if (!NodeList.prototype.forEach) 
+{
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+*/
+
+// Konstruktorfunktion för meddelanden
+var Message = function () {
+    
+    
+};
---
+++ 2-labbymezzage/MessageBoard.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* Lite "tankegångskod" jag tror kommer behöva användas 
+
+// Skapar ny div för ett nytt meddelande, kommer slänga in denna i en loop förmodligen.
+var messageDiv = document.createElement("div");
+div.className = "messageBox";
+
+// Hur jag tänker lite kring hur meddelandesändningsknappen kan tänkas fungera
+var sendMessage = document.getElementById("messageSubmit");
+
+sendMessage.onlick = function (e) {
+e.preventDefault();
+console.log("Tjolahopp, mess sänt!");
+};
+
+*/
