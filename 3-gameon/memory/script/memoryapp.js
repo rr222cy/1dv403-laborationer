828cd09050ae7b9e0272b57bff56318af5930c3f
rr222cy <rr222cy@student.lnu.se>
2014-01-07 13:44:58 +0100

    Objekt, onload och init för Memory

--- 3-gameon/memory/script/memoryapp.js
+++ 3-gameon/memory/script/memoryapp.js
@@ -1,2 +1,17 @@
 /*jslint browser:true */
 "use strict";
+
+var Memory = {
+    
+    randomBricks: [],
+    
+    init:function()
+    {
+        console.log("Appen initierad");
+    }
+    
+};
+
+window.onload = function () {
+    Memory.init();
+};
