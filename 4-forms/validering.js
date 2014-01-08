72f56ba85a0d4d7ef0dda71672b44b0219ffad72
rr222cy <rr222cy@student.lnu.se>
2014-01-08 15:20:11 +0100

    Meddelandehantering, namnvalidering

--- 4-forms/validering.js
+++ 4-forms/validering.js
@@ -9,29 +9,46 @@ var Validator = {
             var firstName = document.getElementById("firstName");
             var lastName = document.getElementById("lastName");
             var submitForm = document.getElementById("submitForm");
+            var error = "";
             var submitAction = function()
             {
+                // Validerar förnamn
                 if(firstName.value === "")
                 {
-                    alert("Tomt");
+                    error = Validator.errorMessage("Fältet får ej lämnas blankt!", "firstNameError");
+                    if(!document.getElementById("firstNameError"))
+                    {
+                        firstName.parentNode.insertBefore(error, firstName.nextSibling);
+                    }                    
                 }
-                else
-                {
-                    alert("Ej tomt");
-                }
+                // Validerar efternamn
                 if(lastName.value === "")
                 {
-                    alert("Tomt");
-                }
-                else
-                {
-                    alert("Ej tomt");
+                    error = Validator.errorMessage("Fältet får ej lämnas blankt!", "lastNameError");
+                    if(!document.getElementById("lastNameError"))
+                    {
+                        lastName.parentNode.insertBefore(error, lastName.nextSibling);
+                    }
                 }
             };
             
             // Vid klick på skicka ska funktionen submitAction köras som validerar formuläret.
             submitForm.addEventListener('click', submitAction, false);            
         },
+    
+    errorMessage:function(message, inputID)
+    {
+        var messageSpan = document.createElement("span");
+        var messageText = document.createTextNode(" " + message);
+        messageSpan.className = "errorMessage";
+        messageSpan.id = inputID;
+        messageSpan.appendChild(messageText);
+        
+        
+        
+        return messageSpan;
+    }
+    
 };
 
 window.onload = function () {
