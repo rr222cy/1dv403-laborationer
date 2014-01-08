c4b3494bb9ecf251234d3e03fe2750bb359d8eb2
rr222cy <rr222cy@student.lnu.se>
2014-01-08 19:17:00 +0100

    Fungerar nu utan JS samt validerar vid blur

--- 4-forms/validering.js
+++ 4-forms/validering.js
@@ -8,24 +8,32 @@ var Validator = {
             // Deklarerar och initierar variablerna/fälten jag kommer arbeta med.
             var firstName = document.getElementById("firstName");
             var lastName = document.getElementById("lastName");
+            var postalCode = document.getElementById("postalCode");
+            var email = document.getElementById("email");
             var submitForm = document.getElementById("submitForm");
             var canSend = false;
             var error = "";
             
-            // Denna funktion körs när användaren vill sända formuläret, validering startas.
+            // Denna funktion körs igång med sidan, validerar utefter att användaren fyller fälten med innehåll.
             var submitAction = function()
             {
-                // Validerar förnamn och efternamn
-                validateName(firstName, "firstName");
-                validateName(lastName, "lastName");
-                
-                // Kontrollerar om alla uppgifter stämmer, sänder isf iväg formuläret.
-                sendForm();
-                
+                // Validerar förnamn, efternamn, postkod samt e-post.
+                firstName.addEventListener('blur',function(e){
+                    validateEmptyFields(firstName, "firstName");
+                }, true);
+                lastName.addEventListener('blur',function(e){
+                    validateEmptyFields(lastName, "lastName");
+                }, true);
+                postalCode.addEventListener('blur',function(e){
+                    validateEmptyFields(postalCode, "postalCode");
+                }, true);
+                email.addEventListener('blur',function(e){
+                    validateEmptyFields(email, "email");
+                }, true);                
             };
             
-            // Funktion för att validera vanlig text, för och efternamn i detta fallet.
-            var validateName = function(inputID, inputName)
+            // Funktion för att validera att inga inputs är tomma.
+            var validateEmptyFields = function(inputID, inputName)
             {
                 var inputErrorMessageID = inputName + "Error";
                 if(inputID.value === "")
@@ -34,6 +42,7 @@ var Validator = {
                     if(!document.getElementById(inputErrorMessageID))
                     {
                         inputID.parentNode.insertBefore(error, inputID.nextSibling);
+                        inputID.className = "inputInvalid";
                     }
                 }
                 else
@@ -44,19 +53,29 @@ var Validator = {
                         {
                             var inputErrorMessageElement = document.getElementById(inputErrorMessageID);
                             inputErrorMessageElement.parentNode.removeChild(inputErrorMessageElement);
-                        }
-                        else 
-                        {
-                            console.log("jag finns inte");
+                            inputID.className = "inputValid";
                         }
                     }
+                    inputID.className = "inputValid";
                 }
+            };
+            
+            // Funktion för att validera postkoder
+            var validatePostalCode = function(inputID, inputName)
+            {
+                validateEmptyFields(inputID, inputName);
+            };
+            
+            // Funktion för att validera e-postadresser
+            var validateEmail = function(inputID, inputName)
+            {
+                validateEmptyFields(inputID, inputName);
             };
             
             // Om allt är validerat ska canSend vara "true" och formuläret kan då sändas iväg. 
             var sendForm = function()
             {               
-                if(!document.getElementById("firstNameError") && !document.getElementById("lastNameError"))
+                if(!document.getElementById("firstNameError") && !document.getElementById("lastNameError") && !document.getElementById("postalCodeError") && !document.getElementById("emailError"))
                 {
                     canSend = true;
                 }
@@ -67,11 +86,20 @@ var Validator = {
                 }
             };
             
-            // Vid klick på skicka ska funktionen submitAction köras som validerar formuläret.
-            submitForm.addEventListener('click', submitAction, false);
-            
+            // Kontrollerar om alla uppgifter stämmer, sänder isf iväg formuläret.
+            submitForm.addEventListener('click', function(e){
+                e.preventDefault();
+                validateEmptyFields(firstName, "firstName");
+                validateEmptyFields(lastName, "lastName");
+                validateEmptyFields(postalCode, "postalCode");
+                validateEmptyFields(email, "email");
+                sendForm();
+            }, false);
+                        
+            submitAction();
         },
     
+    // Funktion för att hantera felmeddelanden och skapa dessa.
     errorMessage:function(message, inputID)
     {
         var messageSpan = document.createElement("span");
