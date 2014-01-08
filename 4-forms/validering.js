24787004dc9d9f720186723ed37650bb3ec48006
rr222cy <rr222cy@student.lnu.se>
2014-01-08 14:33:50 +0100

    Grundläggande logik för valideringen av namn

--- 4-forms/validering.js
+++ 4-forms/validering.js
@@ -1,3 +1,39 @@
 /*jslint browser:true*/
 "use strict";
 
+var Validator = {
+    
+    init:function()
+        {
+            // Deklarerar och initierar variablerna/fälten jag kommer arbeta med.
+            var firstName = document.getElementById("firstName");
+            var lastName = document.getElementById("lastName");
+            var submitForm = document.getElementById("submitForm");
+            var submitAction = function()
+            {
+                if(firstName.value === "")
+                {
+                    alert("Tomt");
+                }
+                else
+                {
+                    alert("Ej tomt");
+                }
+                if(lastName.value === "")
+                {
+                    alert("Tomt");
+                }
+                else
+                {
+                    alert("Ej tomt");
+                }
+            };
+            
+            // Vid klick på skicka ska funktionen submitAction köras som validerar formuläret.
+            submitForm.addEventListener('click', submitAction, false);            
+        },
+};
+
+window.onload = function () {
+    Validator.init();
+};
