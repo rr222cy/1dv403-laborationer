d602d5f2e5e9d10865221464e7967ee65404c7ff
rr222cy <rr222cy@student.lnu.se>
2014-01-08 17:45:48 +0100

    Omstrukturering/snyggare kod för namnkoll

--- 4-forms/validering.js
+++ 4-forms/validering.js
@@ -9,47 +9,53 @@ var Validator = {
             var firstName = document.getElementById("firstName");
             var lastName = document.getElementById("lastName");
             var submitForm = document.getElementById("submitForm");
-            var form = document.getElementById("buyForm");
             var canSend = false;
             var error = "";
+            
+            // Denna funktion körs när användaren vill sända formuläret, validering startas.
             var submitAction = function()
             {
-                // Validerar förnamn.
-                if(firstName.value === "")
+                // Validerar förnamn och efternamn
+                validateName(firstName, "firstName");
+                validateName(lastName, "lastName");
+                
+                // Kontrollerar om alla uppgifter stämmer, sänder isf iväg formuläret.
+                sendForm();
+                
+            };
+            
+            // Funktion för att validera vanlig text, för och efternamn i detta fallet.
+            var validateName = function(inputID, inputName)
+            {
+                var inputErrorMessageID = inputName + "Error";
+                if(inputID.value === "")
                 {
-                    error = Validator.errorMessage("Fältet får ej lämnas blankt!", "firstName");
-                    if(!document.getElementById("firstNameError"))
+                    error = Validator.errorMessage("Fältet får ej lämnas blankt!", inputName);
+                    if(!document.getElementById(inputErrorMessageID))
                     {
-                        firstName.parentNode.insertBefore(error, firstName.nextSibling);
+                        inputID.parentNode.insertBefore(error, inputID.nextSibling);
                     }
                 }
                 else
                 {
-                    if(document.getElementById("firstNameError"))
+                    if(document.getElementById(inputErrorMessageID))
                     {
-                        firstNameError.parentNode.removeChild(firstNameError);
+                        if(inputErrorMessageID)
+                        {
+                            var inputErrorMessageElement = document.getElementById(inputErrorMessageID);
+                            inputErrorMessageElement.parentNode.removeChild(inputErrorMessageElement);
+                        }
+                        else 
+                        {
+                            console.log("jag finns inte");
+                        }
                     }
                 }
-                // Validerar efternamn.
-                if(lastName.value === "")
-                {
-                    error = Validator.errorMessage("Fältet får ej lämnas blankt!", "lastName");
-                    if(!document.getElementById("lastNameError"))
-                    {
-                        lastName.parentNode.insertBefore(error, lastName.nextSibling);
-                    }
-                }
-                else
-                {
-                    if(document.getElementById("lastNameError"))
-                    {
-                        lastNameError.parentNode.removeChild(lastNameError);
-                    }
-                }
-                
-                // Validerar e-post.
-                
-                // Om allt är validerat ska canSend vara "true" och formuläret kan då sändas iväg.                
+            };
+            
+            // Om allt är validerat ska canSend vara "true" och formuläret kan då sändas iväg. 
+            var sendForm = function()
+            {               
                 if(!document.getElementById("firstNameError") && !document.getElementById("lastNameError"))
                 {
                     canSend = true;
