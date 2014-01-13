f1343549bbd6fff9e1781bea80a00edd6f58fd60
rr222cy <rr222cy@student.lnu.se>
2014-01-13 04:05:18 +0100

    Brickor vänds tillbaka korrekt mm

--- 3-gameon/memory/script/memoryapp.js
+++ 3-gameon/memory/script/memoryapp.js
@@ -9,6 +9,8 @@ var Memory = {
     bricksFlipped: [],
     // Håller reda på antalet gissningar.
     bricksGuesses: 0,
+    // Håller reda på antalet par.
+    bricksPair: 0,
     
     // Här ställer vi in hur många kolumner och rader spelet skall ha, satt till 2x2 här.
     cols: 4,
@@ -21,24 +23,17 @@ var Memory = {
     {
         // Slumpar fram brickor genom RandomGenerator och skickar sedan in detta i bricksArray[]
         Memory.bricksArray = RandomGenerator.getPictureArray(Memory.cols, Memory.rows);
-        Memory.renderBoard();
-    },
-    
-    renderBoard:function()
-    {
         // k kommer i iterationen lyfta ut varje slumpat tal ur bricksArray[].
         var k = 0;
         
         for(var i = 0; i < Memory.rows; i++)
-        {
-            
+        {            
             // Det jag gör här är att skjuta in antalet rader i min tabell jag valt att spelet skall ha.
             var tableRow = Memory.gameTable.insertRow();
             for(var j = 0; j < Memory.cols; j++)
             {   
                 // Det jag gör här är att på varje rad skjuta in det antal kolumner jag valt att spelet skall ha per rad.
-                var tableCell = tableRow.insertCell();
-                
+                var tableCell = tableRow.insertCell();               
                 // Här skapar jag en bricka som tilldelas ett sedan tidigare slumpat värde.                
                 createBrick(Memory.bricksArray[k]);
                 k++;
@@ -54,30 +49,40 @@ var Memory = {
             brickImage.width = "24";
             brickImage.height = "24";
             brickImage.alt = "Bilden föreställer en utav memoryts spelbrickor";
-            brickImage.id = nr;
             var brickLink = document.createElement("a");
             brickLink.href = "#";
-            
-            brickLink.onclick = function() 
-            {
-                console.log(nr);
-                brickLink.onclick = null;
-                // Vid klick byter jag ut "frågetecknet" mot rätt bild.
-                brickImage.src = "pics/"+nr+".png";
-                // Lägger till flippade brickan till arrayen som håller koll på matchningar och antalet flippade brickor.
-                checkBrick(nr);
-            };
-            
+            brickLink.id = nr;
             // Lägger min länk kring bilden och skjuter in allt i en kolumn.
             brickLink.appendChild(brickImage);               
             tableCell.appendChild(brickLink);
+            
+            // Anropar min funktion för att flippa brickan, samt kontrollera att brickan inte redan skiftats.
+            flipBrick(brickLink, brickImage, nr);            
+        }
+        
+        function flipBrick(brickLink, brickImage, nr)
+        {
+            brickLink.onclick = function() 
+            {   
+                // Kollar här om min bild är frågetecknet "pics/0.png" om TRUE så ska brickan kunna vändas, annars inte.
+                if(brickImage.getAttribute("src") === "pics/0.png")
+                {
+                    // Vid klick byter jag ut "frågetecknet" mot rätt bild.
+                    brickImage.src = "pics/"+nr+".png";
+                    brickImage.className = "justFlipped";
+                    
+                    // Anropar funktionen för att kolla om vi får ett par eller inte.
+                    checkBrick(nr);
+                }
+                return false;
+            };
         }
         
         function checkBrick(nr)
-        {
+        {   
             // Skickar här in den klickade brickans värde i bricksFlipped-arrayen.
             Memory.bricksFlipped.push(nr);
-            
+            var justFlipped = document.querySelectorAll(".justFlipped");
             
             // Kollar här om vi fått 2 brickor klickade på, om ja så matchas de mot varandra.
             if(Memory.bricksFlipped.length >= 2)
@@ -86,26 +91,43 @@ var Memory = {
                 if(Memory.bricksFlipped[0] === Memory.bricksFlipped[1])
                 {
                     console.log("Brickorna matchar");
+                    justFlipped[0].className = "justFlippedRight";
+                    justFlipped[1].className = "justFlippedRight";
                     Memory.bricksFlipped.length = 0;
                 }
                 else
                 {
                     console.log("Brickorna matchar ej");
-                    Memory.bricksFlipped.length = 0;
+                    // Här sker en timeout på ca ½ sekund innan funktionen för att vända tillbaka brickorna anropas.
+                    setTimeout(function() {
+                        flipBack();
+                    }, 600);                    
                 }
             }
         }
+        
+        function flipBack()
+        {
+            var justFlipped = document.querySelectorAll(".justFlipped");
+            justFlipped[0].src = "pics/0.png";
+            justFlipped[1].src = "pics/0.png";
+            justFlipped[0].className = "";
+            justFlipped[1].className = "";
+            console.log(justFlipped);
+            Memory.bricksFlipped.length = 0;
+            console.log("Flippar tillbaka");
+        }
     },
     
-    memoryMessage:function()
+    memoryMessage:function(message)
     {
         var memoryMessage = document.getElementById("memoryMessage");
-        memoryMessage.innerHTML = "Think I lost my memory.";
+        memoryMessage.innerHTML = message;
     }
     
 };
 
 window.onload = function () {
-    Memory.memoryMessage();
+    Memory.memoryMessage("Think I lost my memory");
     Memory.init();
 };
